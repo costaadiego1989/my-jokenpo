9cbe0ada163df899bae6e1646fbf2b699fd74c24
diego <diego.oliveira@fcamara.com.br>
2022-06-04 00:51:56 -0300

    feat: exibir na tela item escolhido e sorteio da escolha do pc.

--- src/components/jokenpo.jsx
+++ src/components/jokenpo.jsx
@@ -2,14 +2,28 @@ import "./style.css";
 import { FaRegHandRock as Rock } from "react-icons/fa";
 import { FaRegHandPaper as Paper } from "react-icons/fa";
 import { FaRegHandScissors as Scizor } from "react-icons/fa";
-import { useState } from "react";
+import { FaHandRock as Rock2 } from "react-icons/fa";
+import { FaHandPaper as Paper2 } from "react-icons/fa";
+import { FaHandScissors as Scizor2 } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 export const Jokenpo = () => {
-  const [actualChoice, setActualChoice] = useState("userRock");
+  const [actualUserChoice, setActualUserChoice] = useState("");
+  const [actualComputerChoice, setActualComputerChoice] = useState("");
 
   const handleClick = (e) => {
-    console.log(e);
+    if (e === "userRock") setActualUserChoice("userRock");
+    if (e === "userPaper") setActualUserChoice("userPaper");
+    if (e === "userScizor") setActualUserChoice("userScizor");
   };
+
+  let computerChoices = ["computerRock", "computerPaper", "computerScizor"];
+  let computerLetsRoll =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+  useEffect(() => {
+    setActualComputerChoice(computerLetsRoll);
+  }, [])
 
   return (
     <>
@@ -47,42 +61,46 @@ export const Jokenpo = () => {
                 <h3>Sua Pontuação: {0}</h3>
               </div>
             </div>
-            <div className="actualChoice">{}</div>
+            <div className="actualChoice">
+              {actualUserChoice === "userRock" ? (
+                <Rock2 style={{ fontSize: "5rem" }} />
+              ) : null}
+            </div>
+            <div className="actualChoice">
+              {actualUserChoice === "userPaper" ? <Paper2 /> : null}
+            </div>
+            <div className="actualChoice">
+              {actualUserChoice === "userScizor" ? <Scizor2 /> : null}
+            </div>
           </div>
         </div>
         <div className="x">X</div>
         <div className="computerSide">
           <div className="computerSideInformation">
             <div className="computerChoices">
-            <a
-                href
-                role="button"
-                value="pcRock"
-                onClick={() => handleClick("pcRock")}
-              >
-                <Rock />
+              <a href role="button" value="pcRock">
+                {<Rock />}
               </a>
-              <a
-                href
-                role="button"
-                value="pcPaper"
-                onClick={() => handleClick("pcPaper")}
-              >
+              <a href role="button" value="pcPaper">
                 <Paper />
               </a>
-              <a
-                href
-                role="button"
-                value="pcScizor"
-                onClick={() => handleClick("pcScizor")}
-              >
+              <a href role="button" value="pcScizor">
                 <Scizor />
               </a>
             </div>
-            <div className="pcScore">
+            <div className="computerScore">
               <div>
                 <h3>Pontuação do PC: {0}</h3>
               </div>
+            </div>
+            <div className="actualChoice">
+              {actualComputerChoice === "computerRock" ? <Rock2 /> : null}
+            </div>
+            <div className="actualChoice">
+              {actualComputerChoice === "computerPaper" ? <Paper2 /> : null}
+            </div>
+            <div className="actualChoice">
+              {actualComputerChoice === "computerScizor" ? <Scizor2 /> : null}
             </div>
           </div>
         </div>
